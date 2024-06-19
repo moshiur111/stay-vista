a43d711348f11a1eee5c1f7a2b098e40b16de38e
moshiur111 <moshiurrahman4195@gamil.com>
2024-06-19 14:56:35 +0600

    updated few files

--- client/src/components/Rooms/Rooms.jsx
+++ client/src/components/Rooms/Rooms.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
+import Card from "./Card";
+import Container from "../Shared/Container";
 
 const Rooms = () => {
-    return (
-        <div>
-            <h2>Rooms</h2>
-        </div>
-    );
+  const [rooms, setRooms] = useState([]);
+  useEffect(() => {
+    fetch("./rooms.json")
+      .then((res) => res.json())
+      .then((data) => setRooms(data));
+  }, []);
+
+  return (
+    <Container>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {rooms.map((room) => (
+          <Card key={room._id} room={room}></Card>
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default Rooms;
