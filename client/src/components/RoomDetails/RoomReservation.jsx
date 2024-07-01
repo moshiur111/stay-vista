7515bb44caab47ab9b87e62e601c1f01e1b026a5
moshiur111 <moshiurrahman4195@gamil.com>
2024-07-01 10:28:13 +0600

    Bookinginfo and Booking Modal is added

--- client/src/components/RoomDetails/RoomReservation.jsx
+++ client/src/components/RoomDetails/RoomReservation.jsx
@@ -2,13 +2,23 @@ import { formatDistance } from "date-fns";
 import Button from "../Button/Button";
 import Calender from "./Calender";
 import { useState } from "react";
+import BookingModal from "../Modal/BookingModal";
+import useAuth from "../../hooks/useAuth";
 
 const RoomReservation = ({ room }) => {
+  let [isOpen, setIsOpen] = useState(false);
+  const { user } = useAuth();
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
   const [value, setValue] = useState({
-    startDate: new Date(room?.from), 
-    endDate: new Date(room?.to), 
-    key: 'selection'
-  })
+    startDate: new Date(room?.from),
+    endDate: new Date(room?.to),
+    key: "selection",
+  });
+  console.log(value);
 
   const totalDays = parseInt(
     formatDistance(new Date(room?.to), new Date(room?.from)).split(" ")[0]
@@ -16,7 +26,21 @@ const RoomReservation = ({ room }) => {
   // Total price calculation
   const totalPrice = totalDays * room?.price;
 
-  console.log(value);
+  const [bookingInfo, setBookingInfo] = useState({
+    guest: {
+      name: user?.displayName,
+      email: user?.email,
+      image: user?.photoURL,
+    },
+    host: room?.host?.email,
+    location: room?.location,
+    price: totalPrice,
+    from: value.startDate,
+    to: value.endDate,
+    title: room?.title,
+    roomId: room?._id,
+    image: room?.image,
+  });
 
   return (
     <div className="rounded-xl border-[1px] border-neutral-200 overflow-hidden bg-white">
@@ -30,13 +54,15 @@ const RoomReservation = ({ room }) => {
       </div>
       <hr />
       <div className="p-4">
-        <Button label={"Reserve"}></Button>
+        <Button onClick={() => setIsOpen(true)} label={"Reserve"}></Button>
       </div>
       <hr />
       <div className="p-4 flex items-center justify-between font-semibold text-lg">
         <div>Total</div>
         <div>$ {totalPrice}</div>
       </div>
+
+      <BookingModal isOpen={isOpen} closeModal={closeModal} bookingInfo={bookingInfo}></BookingModal>
     </div>
   );
 };
